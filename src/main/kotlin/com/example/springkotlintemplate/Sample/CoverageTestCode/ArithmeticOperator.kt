1b9c4e2db8a2c3caea22ab3a3cfc2012741f3d22
BEOKS <lee01042000@gmail.com>
2022-12-22 15:02:11 +0900

    Fix : 기존에 존재하던 테스트 코드 삭제

--- src/main/kotlin/com/example/springkotlintemplate/Sample/CoverageTestCode/ArithmeticOperator.kt
+++
@@ -1,8 +0,0 @@
-package com.example.springkotlintemplate.Sample.CoverageTestCode
-
-interface ArithmeticOperator {
-    fun plus(a:Int,b:Int): Int
-    fun minus(a:Int,b:Int): Int
-    fun multiply(a:Int,b:Int): Int
-    fun divide(a:Int,b:Int): Int
-}
