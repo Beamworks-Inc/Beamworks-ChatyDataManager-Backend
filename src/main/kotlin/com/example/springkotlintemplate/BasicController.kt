01125f105498f3560308f845f123d22342e7702f
BEOKS <lee01042000@gmail.com>
2022-12-28 14:24:28 +0900

    Test : 샘플 API 를 테스트하기 위한 코드를 작성
    asciidoctor 테스크를 실행하는 과정에서
    지속적으로 에러가 발생해 이를 해결해보고자 실험적인
    코드를 작성함

--- src/main/kotlin/com/example/springkotlintemplate/BasicController.kt
+++ src/main/kotlin/com/example/springkotlintemplate/BasicController.kt
@@ -3,15 +3,18 @@ package com.example.springkotlintemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+data class Response(
+    val description: String
+)
 @RestController
 class BasicController {
 
     @GetMapping("/api")
-    fun getAPI(): String {
-        return "beamworks api"
+    fun getAPI(): Response {
+        return Response("beamworks api")
     }
     @GetMapping("/")
-    fun getBase():String{
-        return "beamworks backend"
+    fun getBase(): Response {
+        return Response("beamworks")
     }
 }
