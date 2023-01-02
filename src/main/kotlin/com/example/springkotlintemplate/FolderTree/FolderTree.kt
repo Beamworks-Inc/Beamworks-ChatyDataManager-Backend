28b9b311cea4468f5deaefbaa48f9d7dec1970ca
BEOKS <lee01042000@gmail.com>
2023-01-02 09:00:09 +0900

    Fix : FolderTreeControllerTest 에서 데이터 파싱 오류 수정
    - FolderTree.kt Dto 에서 생성자를 선언하지 않아서 생긴 버그

--- src/main/kotlin/com/example/springkotlintemplate/FolderTree/FolderTree.kt
+++ src/main/kotlin/com/example/springkotlintemplate/FolderTree/FolderTree.kt
@@ -3,4 +3,6 @@ package com.example.springkotlintemplate.FolderTree
 data class FolderTree(
     val name : String,
     val children : List<FolderTree>
-)
+){
+    constructor() : this("", listOf())
+}
